c91ec6df2e393b10e1983a1c9fcd24035033a180
dmystk <alpha.beta.peta@gmail.com>
2022-01-09 12:15:43 +0900

    Support no-authentication proxy server.

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -2,6 +2,17 @@
 use std::io::{Write, Read};  // need to use stream.read() and stream.write()
 use std::net::TcpStream;
 use url::Url;
+use structopt::StructOpt;
+
+/// Struct for CLI arguments.
+#[derive(Debug, StructOpt)]
+#[structopt(name = "client", about = "Simple CLI HTTP client.")]
+struct Opt {
+    #[structopt(name = "URL")]
+    pub url: Url,
+    #[structopt(short, long, help = "Set proxy server URL")]
+    pub proxy: Option<Url>,
+}
 
 /// Exit program with printing a message to stderr.
 macro_rules! exit_with_message {
@@ -13,44 +24,30 @@ macro_rules! exit_with_message {
 
 fn main() {
     // check command line arguments
-    let args: Vec<String> = std::env::args().collect();
-    if args.len() != 2 {
-        exit_with_message!("Usage: {} <URL>", args[0]);
-    }
-
-    // parse and validate input URL
-    let url = Url::parse(&args[1]).unwrap_or_else(|e| {
-        exit_with_message!("Malformed URL: {}", e);
-    });
+    let opt = Opt::from_args();
+    let url = opt.url;
     if url.scheme() != "http" {
         exit_with_message!("Unsupported shceme: {}", url.scheme());
     }
+    let proxy = opt.proxy;
+    if proxy.is_some() && proxy.as_ref().unwrap().scheme() != "http" {
+        exit_with_message!("Unsupported proxy shceme: {}", proxy.unwrap().scheme());
+    }
 
-    let host = url.host_str().unwrap();
-    let path = url.path();
-    let port = url.port_or_known_default().unwrap();
-
-    // connect to host
-    let mut stream = TcpStream::connect((host, port)).unwrap_or_else(|e| {
-        exit_with_message!("{}", e);
-    });
-
-    // send HTTP GET request
-    let request = format!(concat!(
-        "GET {} HTTP/1.1\r\n",
-        "Host: {}\r\n",
-        "Connection: close\r\n\r\n",
-    ), path, host);
-    stream.write(request.as_bytes()).unwrap_or_else(|e| {
-        exit_with_message!("Failed to send request: {}", e);
-    });
-
-    // recieve response
-    read_chunks(&stream, |chunk| {
-        print!("{}", std::str::from_utf8(chunk).unwrap());
-    }).unwrap_or_else(|e| {
-        exit_with_message!("Failed to recieve response: {}", e);
-    })
+    // request HTTP GET
+    let print_bytes = |bytes: &[u8]| {
+        print!("{}", std::str::from_utf8(bytes).unwrap());
+    };
+    if proxy.is_some() {
+        let proxy_url = proxy.unwrap();
+        request_http_get_with_proxy(&url, &proxy_url, print_bytes).unwrap_or_else(|e| {
+            exit_with_message!("{}", e);
+        });
+    } else {
+        request_http_get(&url, print_bytes).unwrap_or_else(|e| {
+            exit_with_message!("{}", e);
+        });
+    }
 }
 
 /// Unwrap Ok value or terminate function with Err as return value
@@ -63,6 +60,82 @@ macro_rules! unwrap_or_return_err {
             Err(e) => return Err(e),
         }
     };
+}
+
+// Request HTTP GET and process response stream with the callback function.
+// The callback is called chunk by chunk.
+fn request_http_get(url: &Url, callback: fn(&[u8])) -> std::io::Result<()> {
+    // connect to host
+    let host = url.host_str().unwrap();
+    let port = url.port_or_known_default().unwrap();
+    let stream = unwrap_or_return_err!(
+        connect(host, port)
+    );
+
+    // send HTTP GET request
+    let path = url.path();
+    let request = format!(concat!(
+        "GET {} HTTP/1.1\r\n",
+        "Host: {}\r\n",
+        "Connection: close\r\n\r\n",
+    ), path, host);
+    unwrap_or_return_err!(
+        send_request(&stream, &request)
+    );
+
+    // recieve response
+    recieve_response(&stream, callback)
+}
+
+// Request HTTP GET with proxy and process response stream with the callback function.
+// The callback is called chunk by chunk.
+fn request_http_get_with_proxy(
+    url: &Url,
+    proxy_url: &Url,
+    callback: fn(&[u8])
+) -> std::io::Result<()> {
+    // connect to proxy
+    let proxy_host = proxy_url.host_str().unwrap();
+    let proxy_port = proxy_url.port_or_known_default().unwrap();
+    let stream = unwrap_or_return_err!(
+        connect(proxy_host, proxy_port)
+    );
+
+    // send HTTP GET request
+    let host = url.host_str().unwrap();
+    let path = url.as_str();  // need to use the full URL as path when using proxy
+    let request = format!(concat!(
+        "GET {} HTTP/1.1\r\n",
+        "Host: {}\r\n",
+        "Connection: close\r\n\r\n",
+    ), path, host);
+    unwrap_or_return_err!(
+        send_request(&stream, &request)
+    );
+
+    // recieve response
+    recieve_response(&stream, callback)
+}
+
+// Connect to host server.
+fn connect(host: &str, port: u16) -> std::io::Result<TcpStream> {
+    TcpStream::connect((host, port)).map_err(|e| {
+        std::io::Error::new(e.kind(), format!("Failed to connect host: {}", e))
+    })
+}
+
+/// Send a request string to socket.
+fn send_request(mut stream: &TcpStream, request: &str) -> std::io::Result<usize> {
+    stream.write(request.as_bytes()).map_err(|e| {
+        std::io::Error::new(e.kind(), format!("Failed to send request: {}", e))
+    })
+}
+
+/// Recieve response chunk by chunk.
+fn recieve_response(stream: &TcpStream, callback: fn(&[u8])) -> std::io::Result<()> {
+    read_chunks(stream, callback).map_err(|e| {
+        std::io::Error::new(e.kind(), format!("Failed to recieve response: {}", e))
+    })
 }
 
 /// Maximum size of chunk.
